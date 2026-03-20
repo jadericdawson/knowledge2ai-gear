508bfaf78be968729b29f6ef1e8fa9ee6256a28d
Jaderic Dawson <jadericdawson@gmail.com>
2026-03-20 14:34:45 -0400

    config: set site URL to k2ai.shop

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://jadericdawson.com',
+  site: 'https://k2ai.shop',
   integrations: [tailwind()],
   compressHTML: true,
   build: {
